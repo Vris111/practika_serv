10b98ca18599d2ff15e7481f0ac9db843968e4b3
Vris111 <ruslansimanovic56@mail.ru>
2024-03-25 19:03:05 +0700

    clear _

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -8,7 +8,6 @@ class Site
         $view = new View();
         return $view->render('site.hello', ['message' => 'index working']);
     }
-
     public function hello(): string
     {
         return new View('site.hello', ['message' => 'hello working']);
