60ed5932142592fb98e8bc99661d244883baeb06
Vris111 <ruslansimanovic56@mail.ru>
2024-03-26 22:59:40 +0700

    add identity interface

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -13,4 +13,9 @@ function getConfigs(string $path = DIR_CONFIG): array
     return $settings;
 }
 require_once __DIR__ . '/../routes/web.php';
-return new Src\Application(new Src\Settings(getConfigs()));
+$app = new Src\Application(new Src\Settings(getConfigs()));
+function app() {
+    global $app;
+    return $app;
+}
+return $app;
