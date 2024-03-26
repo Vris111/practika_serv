ca9206894659027e44f26fb9364defcfe0d4e98c
Vris111 <ruslansimanovic56@mail.ru>
2024-03-26 21:56:54 +0700

    add db and create some posts

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -1,12 +1,13 @@
 <?php
 namespace Controller;
+use Model\Post;
 use Src\View;
 class Site
 {
     public function index(): string
     {
-        $view = new View();
-        return $view->render('site.hello', ['message' => 'index working']);
+        $posts = Post::all();
+        return (new View())->render('site.post', ['posts' => $posts]);
     }
     public function hello(): string
     {
