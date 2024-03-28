4be797365753a1568b3ae7d8b0aefbec2f27a143
Vris111 <ruslansimanovic56@mail.ru>
2024-03-28 09:07:08 +0700

    acces for wsr

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 return [
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'mvc',
-    'username' => 'root',
-    'password' => '',
+    'database' => 'afvfihvi_m1',
+    'username' => 'afvfihvi',
+    'password' => 'iCTTPy',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
