2c02e2c5f438d4259b34fd0f3eb6e29e138832af
Vris111 <ruslansimanovic56@mail.ru>
2024-03-28 13:14:05 +0700

    style done

--- views/site/login.php
+++ views/site/login.php
@@ -1,4 +1,4 @@
-<h2 class="topic_login">Авторизация</h2>
+<h1 class="topic" >Авторизация</h2>
 <h3><?= $message ?? ''; ?></h3>
 
 <h3><?= app()->auth->user()->name ?? ''; ?></h3>
@@ -6,8 +6,8 @@
 if (!app()->auth::check()):
     ?>
     <form method="post" class="login_form">
-        <label>Логин <input class="inp_login" type="text" name="login"></label>
+        <label>Логин  <input class="inp_login" style="width: 100px" type="text" name="login"></label>
         <label>Пароль <input class="inp_login" type="password" name="password"></label>
-        <button class="btn">Войти</button>
+        <button class="login_btn">Войти</button>
     </form>
 <?php endif;
--- views/site/signup.php
+++ views/site/signup.php
@@ -1,8 +1,8 @@
-<h2>Регистрация нового сисадмина</h2>
+<h1 class="topic">Регистрация нового сисадмина</h2>
 <h3><?= $message ?? ''; ?></h3>
-<form method="post">
-    <label>Имя <input type="text" name="name"></label>
-    <label>Логин <input type="text" name="login"></label>
-    <label>Пароль <input type="password" name="password"></label>
-    <button>Зарегистрироваться</button>
+<form method="post" class="login_form">
+    <label>Имя <input style="height: 30px; background-color: #ffff; font-size: 16px; width: 230px" type="text" name="name"></label>
+    <label>Логин <input style="height: 30px; background-color: #ffff; font-size: 16px; width: 210px" type="text" name="login"></label>
+    <label>Пароль <input class="inp_login" type="password" name="password"></label>
+    <button class="login_btn">Зарегистрировать</button>
 </form>
