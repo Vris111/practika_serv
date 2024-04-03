d0bba032d014fbca851a589b5b543d950fc08639
Vris111 <ruslansimanovic56@mail.ru>
2024-04-03 10:29:11 +0700

    api auth

--- app/Controller/Api.php
+++ app/Controller/Api.php
@@ -3,6 +3,7 @@
 namespace Controller;
 
 use Model\Post;
+use Model\User;
 use Src\Request;
 use Src\View;
 
@@ -18,5 +19,26 @@ class Api
     public function echo(Request $request): void
     {
         (new View())->toJSON($request->all());
+    }
+
+    public function authorize(Request $request): void
+    {
+        $current_user = $request->all();
+
+        $user = User::where(['login' => $current_user['login'],
+            'password' => $current_user['password']])->first();
+
+        if(!empty($user)){
+            $characters = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $charactersLength = strlen($characters);
+            $token = '';
+            for($i = 0; $i < $charactersLength; $i++){
+                $token .= $characters[rand(0, $charactersLength - 1)];
+            }
+            (new View())->toJSON(['token' => $token]);
+        }
+        else {
+            (new View())->toJSON(['error' => 'Invalid username or password']);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Src\Route;
 
 Route::add('GET', '/', [Controller\Api::class, 'index']);
 Route::add('POST', '/echo', [Controller\Api::class, 'echo']);
+Route::add('POST', '/authorize', [Controller\Api::class, 'authorize']);
